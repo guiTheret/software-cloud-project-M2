6b01ff156985bdca12b6ed8b9fb4960c50e2c5fe
guiTheret <gtheret@sfr.fr>
2024-01-27 16:37:34 +0000

    UPPPPPP

---
+++ api/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+// allow CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  console.log(users);
+  return res.status(200).send({ ok: true, data: users });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,83 +1,38 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useState, useEffect } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
-//tessssss
-import Header from "./components/Header/Header";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import Home from "./components/Home";
-import Account from "./components/Account/Account";
-import CleanerBook from "./components/Cleaner/CleanerBook";
+import { useEffect, useState } from "react";
 
-import api from "./api/api";
 import "./App.css";
 
 function App() {
-  const [isAuth, setIsAuth] = useState<boolean>(false);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [userInfo, setUserInfo] = useState<userInfo>({
-    username: "",
-    isCleaner: 0,
-    email: "",
-    id: -1,
-  });
-  const navigate = useNavigate();
+  const [count, setCount] = useState(0);
+  const [users, setUsers] = useState([]);
 
-  const fetchAuth = async () => {
-    try {
-      const res = await api.get("/auth/login");
-      setUserInfo(res.data.userInfo);
-      setIsAuth(true);
-      setLoading(false);
-    } catch (err) {
-      navigate("/login");
-      setIsAuth(false);
-      setLoading(false);
-    }
+  const [loading, setLoading] = useState(false);
+
+  const fetchUsers = async () => {
+    setLoading(true);
+    const url = import.meta.env.VITE_API_URL + "/users";
+    console.log(url);
+    const res = await fetch(url);
+    const { data } = await res.json();
+    setUsers(data);
+    setLoading(false);
   };
+
   useEffect(() => {
-    fetchAuth();
+    fetchUsers();
   }, []);
 
-  const onAuthSuccess = () => {
-    setIsAuth(true);
-    // to refresh userInfo
-    fetchAuth();
-    navigate("/home");
-  };
+  if (loading) return <div>Loading...</div>;
 
-  const logOut = () => {
-    setIsAuth(false);
-    navigate("/login");
-  };
-
-  if (loading) {
-    return <></>;
-  } else {
-    return (
-      <div>
-        <Header isAuth={isAuth} onSuccess={() => logOut()} />
-        <Routes>
-          <Route path="/home" element={<Home isAuth={isAuth} />} />
-          <Route
-            path="/register"
-            element={<Register onSuccess={() => onAuthSuccess()} />}
-          />
-          <Route
-            path="/login"
-            element={<Login onSuccess={() => onAuthSuccess()} />}
-          />
-          <Route
-            path="/account"
-            element={
-              <Account isCleaner={userInfo.isCleaner} userInfo={userInfo} />
-            }
-          />
-          <Route path="/book" element={<CleanerBook />} />
-        </Routes>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <ul>
+        {users.map((user: any) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
