5568752c469772eafd496fa49b1276439788f3ad
guiTheret <gtheret@sfr.fr>
2024-01-28 23:58:34 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello World! project software cloud Guillaume et Enrique");
+  res.send("Hello World! project software cloud Guillaume et Enrique a");
 });
 
 app.get("/users", async (req, res) => {
