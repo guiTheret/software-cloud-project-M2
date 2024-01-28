7cfccd35fd2d05e2b2a4f547a899850b8e603040
guiTheret <gtheret@sfr.fr>
2024-01-28 18:21:19 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -44,7 +44,6 @@ app.get("/fakeusers", async (req, res) => {
     const createdUsers = await prisma.user.createMany({
       data: fakeUsers,
     });
-    //
 
     return res.status(200).send({ ok: true, data: createdUsers });
   } catch (error) {
