db631c5ffa89c85239b9c0f84a514b53ef88e11f
guiTheret <gtheret@sfr.fr>
2024-01-28 18:22:13 +0000

    Update index.js

--- api/index.js
+++ api/index.js
@@ -45,6 +45,8 @@ app.get("/fakeusers", async (req, res) => {
       data: fakeUsers,
     });
 
+    //fix
+
     return res.status(200).send({ ok: true, data: createdUsers });
   } catch (error) {
     console.error("Error creating fake users:", error);
