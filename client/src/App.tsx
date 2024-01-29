e8355d0b9207ee43fc529fa394f7dfa2bf212512
guiTheret <gtheret@sfr.fr>
2024-01-29 10:15:10 +0000

    up

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,7 +20,9 @@ function App() {
   const fetchUsers = async () => {
     setLoading(true);
 
-    const { data } = await axios.get<User[]>("/api/users");
+    const { data } = await axios.get<User[]>(
+      import.meta.env.API_URL + "/users"
+    );
 
     setUsers(data);
 
